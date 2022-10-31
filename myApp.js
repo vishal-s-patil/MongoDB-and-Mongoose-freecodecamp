5345de9e84098d243191a08dbda520d732c2a01e
Vishal Patil <78012271+vishal-s-patil@users.noreply.github.com>
2022-10-31 23:53:02 +0530

    make it better

--- myApp.js
+++ myApp.js
@@ -40,9 +40,11 @@ const createManyPeople = (arrayOfPeople, done) => {
   })
 };
 
-
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, function (err, data) {
+    if (err) done(err, null);
+    done(null, data);
+  })
 };
 
 const findOneByFood = (food, done) => {
